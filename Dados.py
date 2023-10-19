2bfd35fb2583cdc1c8b1cddf7a4dabf38a469381
edilson gomes de lima <edskateicevida@gmail.com>
2023-10-19 20:07:01 -0300

    00pontopython

---
+++ Dados.py
@@ -0,0 +1,11 @@
+# Define your data
+name = "John Doe"
+age = 30
+email = "johndoe@example.com"
+country = "United States"
+
+# Display your data
+print("Name:", name)
+print("Age:", age)
+print("Email:", email)
+print("Country:", country)
