56149cdd316f6ff71ce379a399b47eb3d37ee2d9
edilson gomes de lima <edskateicevida@gmail.com>
2023-11-13 15:37:06 -0300

    add . 000

---
+++ table01.py
@@ -0,0 +1,13 @@
+# Basic Python Program with IDE Name
+
+# Get user input for name
+name = input("Enter your name: ")
+
+# Set the IDE name (you can change this to your preferred IDE)
+ide_name = "Your IDE"
+
+# Print a personalized message using the entered name and IDE name
+print("Hello, " + name + "! Welcome to " + ide_name + ".")
+
+# Add a final message
+print("Thank you for using this program!")
